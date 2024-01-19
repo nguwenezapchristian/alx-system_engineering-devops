338087ce89030f6cba230595affbaa0057001a85
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-19 12:55:46 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,16 @@
 # install flask from pip3
 
+exec{ 'apt-get-update':
+  command => 'usr/bin/apt-get update',
+}
+
+exec{ 'install-python3-pip':
+  command => 'usr/bin/apt-get install -y python3-pip',
+  require => Exec['apt-get-update'],
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+  require  => Exec['install-python3-pip'],
 }
