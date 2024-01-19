9ab336ed6ed5cce408b7bfc39b824a6d41ff2cba
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-19 12:22:44 +0200

    changed the version to install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Use an exec resource to install flask from pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.1',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/bin',
   creates => 'usr/bin/flask',
 }
