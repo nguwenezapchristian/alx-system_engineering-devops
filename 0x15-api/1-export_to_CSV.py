bbf2d13bec166bbe7dc4ceb285b01f999ca1f9b6
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-02-19 21:45:51 +0200

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = sys.argv[1]
+    user = requests.get(url + f"users/{userId}").json()
+    todos = requests.get(url + f"todos?userId={userId}").json()
+    USERNAME_NAME = user.get("username")
+    with open(f"{userId}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow(
+                    [
+                        userId,
+                        USERNAME_NAME,
+                        task.get("completed"),
+                        task.get("title")
+                        ]
+                    )
