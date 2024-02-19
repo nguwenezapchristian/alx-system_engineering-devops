92c962bba58e6476f37076cece58f6ecec888447
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-02-19 22:51:47 +0200

    resolved E124 error which is indentation

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -18,6 +18,5 @@ if __name__ == "__main__":
                         "task": task.get("title"),
                         "completed": task.get("completed"),
                         "username": USERNAME_NAME
-                        }]
-                        }
-                    ,jsonfile)
+                        }]},
+                    jsonfile)
