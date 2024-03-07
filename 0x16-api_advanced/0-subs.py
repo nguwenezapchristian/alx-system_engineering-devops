2cfceb091afc478dc60ec375d8967b8dc7b8c93f
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-07 23:31:32 +0200

    quick fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ a function that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0 """
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,14 +18,11 @@ def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {"User-Agent": "Chris-User-Agent"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        if data:
-            for post in data:
-                print(post['data']['title'])
-        else:
-            print("No posts found in the subreddit.")
+    sub_info = requests.get(url,
+                            headers=headers,
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        print("None")
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
