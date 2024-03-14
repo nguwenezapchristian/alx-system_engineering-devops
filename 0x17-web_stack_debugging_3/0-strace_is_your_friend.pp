bbb1b97c88fd6b33f668e644ea5242039c93dc36
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-14 20:58:31 +0200

    Wordpress website running on a LAMP stack

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# The web stack you are debugging today is a Wordpress
+# website running on a LAMP stack
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
