68befa83c07ba94621927aeb3540b9da7e833965
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-18 10:02:18 +0200

    return 0 when the subreddit is invalid

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,6 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "Chris-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
+    if response.status_code == 404:
         return 0
     return response.json().get('data', {}).get('subscribers')
