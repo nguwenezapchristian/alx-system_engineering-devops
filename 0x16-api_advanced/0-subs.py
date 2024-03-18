e275f824aafb3309b3c53111c1f1c56c004ed42d
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-18 09:09:18 +0200

    does not return 0 when the key is false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,4 +11,4 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 300:
         return 0
-    return response.json().get('data', {}).get('subscribers', 0)
+    return response.json().get('data', {}).get('subscribers')
