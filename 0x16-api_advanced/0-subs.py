7819ceb9071a44c9ef15da6f59abe1fb5f749c2b
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-21 20:28:39 +0200

    included documentation in the function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    a function that queries the Reddit
+    API and returns the number of subscribers
+    """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "0x16-api_advanced (nguwenezacc982@gmail.com)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
