ca01a0a44df9e272f5916aafbceaa4419c9715ea
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-03-21 20:42:07 +0200

    if 404 code return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "0x16-api_advanced (nguwenezacc982@gmail.com)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 400:
+    if response.status_code == 404:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
