78090bb20fdd474e300324f2a9b473f5fd4cfc46
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-03-21 20:39:10 +0200

    when no subreddit provided return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,8 @@ def number_of_subscribers(subreddit):
     a function that queries the Reddit
     API and returns the number of subscribers
     """
+    if not subreddit:
+        return 0
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "0x16-api_advanced (nguwenezacc982@gmail.com)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
